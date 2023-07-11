9d1eb303585685a3ada65c4b4f67131609ea63cf
mechanical118 <thisislee1@naver.com>
2023-07-12 07:15:23 +0900

    7월12일 새벽 폴더 기능 구현
    응애

--- lib/Controller/Project_Tab_Controller.dart
+++ lib/Controller/Project_Tab_Controller.dart
@@ -1,3 +1,179 @@
-import 'package:get/get.dart';
+import 'dart:convert';
 
-class ProjectTabController extends GetxController {}
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:front_end/Component/Config.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:fluent_ui/fluent_ui.dart';
+
+class ProjectTabController extends GetxController {
+  List<TreeViewItem> totalfolders = [];
+  RxList<TreeViewItem> firstFolders = <TreeViewItem>[].obs;
+  Rx<int> droptest = 0.obs;
+
+  @override
+  void onInit() {
+    makeFolderListInfo(example);
+    super.onInit();
+  }
+
+  ///백엔드 데이터를 받는 부분 추가후 작동
+  void makeFolderListInfo(List<dynamic> data) {
+    final List<int> stack = [];
+    final List<TreeViewItem> folders = [];
+    List<TreeViewItem> rootFolders = [];
+    for (int i = 0; i < data.length; i++) {
+      final item = data[i];
+      final int id = item['id'];
+      final String name = item['name'];
+      final int? parentId = item['parent_id'];
+
+      while (stack.isNotEmpty && stack.last != parentId) {
+        stack.removeLast();
+      }
+
+      final folder = makeFolderItem(name, id, parentId);
+
+      if (stack.isNotEmpty) {
+        TreeViewItem parentFolder = folders.firstWhere((element) {
+          return element.value["id"] == parentId;
+        });
+        parentFolder.children.add(folder);
+      } else {
+        rootFolders.add(folder);
+      }
+      stack.add(id);
+      folders.add(folder);
+    }
+    totalfolders.addAll(folders);
+    firstFolders.addAll(rootFolders);
+  }
+
+  TreeViewItem makeFolderItem(String name, int id, int? parent) {
+    return TreeViewItem(
+        leading: const Icon(FluentIcons.fabric_folder),
+        expanded: false,
+        children: [],
+        value: {"parent": parent, "id": id},
+        content: Draggable(
+          data: {"parent": parent, "id": id},
+          feedback: Container(
+            color: Colors.grey.withOpacity(0.3),
+            width: 140,
+            height: 30,
+            child: Center(
+              child: Text(
+                name,
+                style: const TextStyle(color: Colors.black, fontSize: 12),
+              ),
+            ),
+          ),
+          child: DragTarget(
+            builder: (BuildContext context, List<dynamic> candidateData,
+                List<dynamic> rejectedData) {
+              return Text(name);
+            },
+            onWillAccept: (Map<dynamic, dynamic>? data) {
+              if (data!["id"] != id) {
+                return true;
+              } else {
+                return false;
+              }
+            },
+            onAccept: (Map<String, dynamic> data) async {
+              //삭제할 부분, 뒷 부분에 이미 있으며 성능 테스트를 위해 앞에도 뒀음.
+              TreeViewItem targetFolder = totalfolders
+                  .firstWhere((element) => element.value["id"] == data["id"]);
+              totalfolders
+                  .firstWhere((element) => element.value["id"] == id)
+                  .children
+                  .add(targetFolder);
+
+              if (data["parent"] != null) {
+                TreeViewItem parentItem = totalfolders.firstWhere(
+                    (element) => element.value["id"] == data["parent"]);
+                parentItem.children.removeWhere(
+                    (element) => element.value["id"] == data["id"]);
+              } else {
+                firstFolders.removeWhere(
+                    (element) => element.value["id"] == data["id"]);
+              }
+              data["parent"] = id;
+              firstFolders.refresh();
+              const storage = FlutterSecureStorage();
+
+              final url =
+                  Uri.parse('http://$HOST/api/data/update_database_directory');
+              final Map<String, dynamic> requestBody = {
+                "uid": await storage.read(key: "uid"),
+                "access_token": await storage.read(key: "access_token"),
+                "refresh_token": await storage.read(key: "refresh_token"),
+                "target_database_id": data["id"],
+                "destination_database_id": parent
+              };
+              final headers = {"Content-type": "application/json"};
+
+              final response = await http.post(
+                url,
+                headers: headers,
+                body: jsonEncode(requestBody),
+              );
+              if (response.statusCode == 200) {
+                TreeViewItem targetFolder = totalfolders
+                    .firstWhere((element) => element.value["id"] == data["id"]);
+                totalfolders
+                    .firstWhere((element) => element.value["id"] == id)
+                    .children
+                    .add(targetFolder);
+
+                if (data["parent"] != null) {
+                  TreeViewItem parentItem = totalfolders.firstWhere(
+                      (element) => element.value["id"] == data["parent"]);
+                  parentItem.children.removeWhere(
+                      (element) => element.value["id"] == data["id"]);
+                } else {
+                  firstFolders.removeWhere(
+                      (element) => element.value["id"] == data["id"]);
+                }
+                data["parent"] = id;
+                firstFolders.refresh();
+              } else {}
+            },
+          ),
+        ));
+  }
+}
+
+//정보를 {아이디, name, 부모 아이디} 형태로 받은 list가 존재한다고 가정. 다음은 예시 데이터. 로그인 즉시 or 프로젝트 선택시 백엔드로부터 폴더 데이터 받기
+List<dynamic> example = [
+  {
+    "id": 1,
+    "name": "하이탑",
+    "parent_id": null,
+  },
+  {
+    "id": 2,
+    "name": "고1",
+    "parent_id": 1,
+  },
+  {
+    "id": 3,
+    "name": "고2",
+    "parent_id": 1,
+  },
+  {
+    "id": 4,
+    "name": "중간대비",
+    "parent_id": 3,
+  },
+  {
+    "id": 5,
+    "name": "기말대비",
+    "parent_id": 3,
+  },
+  {
+    "id": 6,
+    "name": "고3",
+    "parent_id": 1,
+  },
+];
--- lib/Screen/Project_Tab_Screen.dart
+++ lib/Screen/Project_Tab_Screen.dart
@@ -1,30 +1,254 @@
+import 'dart:convert';
+
 import 'package:fluent_ui/fluent_ui.dart';
-import 'package:front_end/Component/Folder_List.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:front_end/Component/Config.dart';
+import 'package:front_end/Controller/Project_Tab_Controller.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 class ProjectTabScreen extends StatelessWidget {
-  List<Folder> folderList = makeFolderListInfo(example);
-
   ProjectTabScreen({super.key});
-
+  final _flyoutcontroller = FlyoutController();
+  final TextEditingController _textcontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return ScaffoldPage(
-      content: Row(
-        children: [
-          Expanded(
-            flex: 1,
-            child: SizedBox(
-                height: double.infinity,
-                child: Expanded(child: FolderList(folders: folderList))),
-          ),
-          Expanded(
-            flex: 4,
-            child: Container(
-              color: Colors.teal,
+    Get.put(ProjectTabController());
+
+    return GetX<ProjectTabController>(
+      builder: (controller) {
+        return Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 1,
+              child: FlyoutTarget(
+                controller: _flyoutcontroller,
+                child: TreeView(
+                  onSecondaryTap: (item, details) {
+                    const storage = FlutterSecureStorage();
+                    _flyoutcontroller.showFlyout(builder: (context) {
+                      return MenuFlyout(
+                        items: [
+                          MenuFlyoutItem(
+                            text: const Text("폴더 삭제"),
+                            onPressed: () async {
+                              //삭제할 부분, 뒷부분에 있으며 성능 확인을 위해 놔둠.
+                              if (item.value["parent"] != null) {
+                                Navigator.pop(context);
+                                TreeViewItem deleteTargetParent = controller
+                                    .totalfolders
+                                    .firstWhere((element) =>
+                                        item.value["parent"] ==
+                                        element.value["id"]);
+                                deleteTargetParent.children.remove(item);
+                                TreeViewItem deleteTarget = controller
+                                    .totalfolders
+                                    .firstWhere((element) => item == element);
+                              } else {
+                                controller.firstFolders
+                                    .removeWhere((element) => item == element);
+                              }
+                              controller.firstFolders.refresh();
+                              final url =
+                                  Uri.parse('http://$HOST/delete_database');
+                              final Map<String, dynamic> requestBody = {
+                                "uid": await storage.read(key: "uid"),
+                                "access_token":
+                                    await storage.read(key: "access_token"),
+                                "refresh_token":
+                                    await storage.read(key: "refresh_token"),
+                                "delete_database_id": item.value["id"]
+                              };
+                              final headers = {
+                                "Content-type": "application/json"
+                              };
+
+                              final response = await http.post(
+                                url,
+                                headers: headers,
+                                body: jsonEncode(requestBody),
+                              );
+                              if (response.statusCode == 200) {
+                                if (item.value["parent"] != null) {
+                                  TreeViewItem deleteTargetParent = controller
+                                      .totalfolders
+                                      .firstWhere((element) =>
+                                          item.value["parent"] ==
+                                          element.value["id"]);
+                                  deleteTargetParent.children.remove(item);
+                                  TreeViewItem deleteTarget = controller
+                                      .totalfolders
+                                      .firstWhere((element) => item == element);
+                                } else {
+                                  controller.firstFolders.removeWhere(
+                                      (element) => item == element);
+                                }
+                                controller.firstFolders.refresh();
+                                displayInfoBar(
+                                  context,
+                                  builder: (context, close) {
+                                    return InfoBar(
+                                      severity: InfoBarSeverity.success,
+                                      title: const Text('폴더 삭제 성공'),
+                                      action: IconButton(
+                                        icon: const Icon(FluentIcons.clear),
+                                        onPressed: close,
+                                      ),
+                                    );
+                                  },
+                                );
+                              } else {
+                                displayInfoBar(
+                                  context,
+                                  builder: (context, close) {
+                                    return InfoBar(
+                                      severity: InfoBarSeverity.warning,
+                                      title: const Text('폴더 삭제 실패'),
+                                      action: IconButton(
+                                        icon: const Icon(FluentIcons.clear),
+                                        onPressed: close,
+                                      ),
+                                    );
+                                  },
+                                );
+                              }
+                            },
+                          ),
+                          const MenuFlyoutSeparator(),
+                          MenuFlyoutItem(
+                            text: const Text("이름 바꾸기"),
+                            onPressed: () {
+                              //추가할 부분, 폴더 renaming관련 백엔드와 협업 후 기능 구현
+                            },
+                          ),
+                          const MenuFlyoutSeparator(),
+                          MenuFlyoutItem(
+                              text: const Text("새폴더"),
+                              onPressed: () async {
+                                Navigator.pop(context);
+                                await showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return ContentDialog(
+                                        title: const Text("새폴더 이름을 정해주세요"),
+                                        content: TextBox(
+                                          controller: _textcontroller,
+                                        ),
+                                        actions: [
+                                          Button(
+                                            child: const Text("취소"),
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                          ),
+                                          FilledButton(
+                                            child: const Text('확인'),
+                                            onPressed: () async {
+                                              //추후 백엔드에서 받은뒤, 중앙 인자값 변경 및 뒤 순서로 미루기
+                                              Navigator.pop(context);
+                                              TreeViewItem newFolder =
+                                                  controller.makeFolderItem(
+                                                      _textcontroller.text,
+                                                      555,
+                                                      item.value["id"]);
+                                              item.children.add(newFolder);
+                                              controller.firstFolders.refresh();
+
+                                              final url = Uri.parse(
+                                                  'http://$HOST/api/auth/create_database');
+                                              final Map<String, dynamic>
+                                                  requestBody = {
+                                                "uid": await storage.read(
+                                                    key: "uid"),
+                                                "access_token": await storage
+                                                    .read(key: "access_token"),
+                                                "refresh_token": await storage
+                                                    .read(key: "refresh_token"),
+                                                "name": _textcontroller.text,
+                                                "parent_id":
+                                                    item.value["parent"]
+                                              };
+                                              final headers = {
+                                                "Content-type":
+                                                    "application/json"
+                                              };
+
+                                              final response = await http.post(
+                                                url,
+                                                headers: headers,
+                                                body: jsonEncode(requestBody),
+                                              );
+
+                                              if (response.statusCode == 200) {
+                                                //수정해야함. 백엔드로부터 새로만든 폴더 id 받아야함.
+                                                displayInfoBar(
+                                                  context,
+                                                  builder: (context, close) {
+                                                    return InfoBar(
+                                                      severity: InfoBarSeverity
+                                                          .success,
+                                                      title: const Text(
+                                                          '폴더 만들기 성공'),
+                                                      action: IconButton(
+                                                        icon: const Icon(
+                                                            FluentIcons.clear),
+                                                        onPressed: close,
+                                                      ),
+                                                    );
+                                                  },
+                                                );
+                                              } else {
+                                                displayInfoBar(
+                                                  context,
+                                                  builder: (context, close) {
+                                                    return InfoBar(
+                                                      severity: InfoBarSeverity
+                                                          .warning,
+                                                      title: const Text(
+                                                          '폴더 만들기 실패'),
+                                                      action: IconButton(
+                                                        icon: const Icon(
+                                                            FluentIcons.clear),
+                                                        onPressed: close,
+                                                      ),
+                                                    );
+                                                  },
+                                                );
+                                              }
+                                            },
+                                          ),
+                                        ],
+                                      );
+                                    });
+                              }),
+                        ],
+                      );
+                    });
+                  },
+                  onItemInvoked: (item, reason) async {
+                    //클릭했을 때 넣을 함수 추가
+
+                    controller.droptest.value = item.value["id"];
+                  },
+                  items: controller.firstFolders,
+                ),
+              ),
             ),
-          )
-        ],
-      ),
+            Expanded(
+              flex: 4,
+              child: Center(
+                child: Container(
+                  color: Colors.yellow,
+                  width: 300,
+                  height: 300,
+                  child: Text(controller.droptest.value.toString()),
+                ),
+              ),
+            )
+          ],
+        );
+      },
     );
   }
 }

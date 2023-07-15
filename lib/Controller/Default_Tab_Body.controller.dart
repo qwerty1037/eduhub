f726bdf92a847ba2f367d6c32258a411ae9175fa
mechanical118 <thisislee1@naver.com>
2023-07-15 22:33:48 +0900

    Update Default_Tab_Body.controller.dart

--- lib/Controller/Default_Tab_Body.controller.dart
+++ lib/Controller/Default_Tab_Body.controller.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 
 class DefaultTabBodyController extends GetxController {
   Rx<Widget> workingSpaceWidget = Container(
-    color: Colors.teal,
+    color: Colors.white,
+    child: const Center(child: Text("폴더 또는 기능을 선택해주세요")),
   ).obs;
 }

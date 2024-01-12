cd63a9f9d1a3096645827e3c74823bf125444522
mechanical118 <thisislee1@naver.com>
2024-01-12 12:11:57 +0900

    대소문자

---
+++ lib/Screen/home_tabview.dart
@@ -0,0 +1,38 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:front_end/Controller/Tab_Controller.dart';
+import 'package:get/get.dart';
+
+class HomeTabView extends StatelessWidget {
+  const HomeTabView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldPage(
+      padding: EdgeInsets.zero,
+      content: GetX<TabController>(
+        builder: (controller) {
+          return TabView(
+            minTabWidth: 0,
+            currentIndex: controller.currentTabIndex.value,
+            tabs: controller.tabs,
+            tabWidthBehavior: TabWidthBehavior.sizeToContent,
+            closeButtonVisibility: CloseButtonVisibilityMode.always,
+            onChanged: (index) {
+              controller.currentTabIndex.value = index;
+            },
+            onNewPressed: () {
+              controller.onNewPressed();
+            },
+            onReorder: (oldIndex, newIndex) {
+              if (oldIndex * newIndex > 0) {
+                controller.onReorder(oldIndex, newIndex);
+              } else {
+                null;
+              }
+            },
+          );
+        },
+      ),
+    );
+  }
+}
